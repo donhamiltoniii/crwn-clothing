d493912f5a18a4876a5f479577c369bc0e96625e
Don Hamilton III <donhamiltoniii@gmail.com>
2019-11-05 19:20:35 -0500

    Add google auth

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -52,8 +52,10 @@ class SignIn extends React.Component {
             value={this.state.password}
           />
 
-          <CustomButton type="submit">Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>Sign in with Google</CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">Sign in</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+          </div>
         </form>
       </div>
     )
